bf12c46d5030c1e4903ad440f1b2cf4ee7b7da62
İlyas AKTAŞ <77029598+aktasilyas@users.noreply.github.com>
2024-03-03 14:07:02 +0300

    Listview eklendi

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,25 +1,33 @@
-
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
-class Expenses extends StatefulWidget{
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
   @override
   State<Expenses> createState() {
-
-return _ExpensesState();
+    return _ExpensesState();
   }
 }
 
-class _ExpensesState extends State<Expenses>{
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _reqisteredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
-        children:  [
-          Text('The Chart...'),
-          Text('Expenses list...')
-        ],
+        children: [Text('The Chart...'), Text('Expenses list...')],
       ),
     );
   }
