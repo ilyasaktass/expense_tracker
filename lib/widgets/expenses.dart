adb18a915a05b1f76231ba2d3d8544a3be008411
İlyas AKTAŞ <77029598+aktasilyas@users.noreply.github.com>
2024-03-09 00:14:09 +0300

    modal eklendi tarihler formatlandı

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,8 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -23,11 +26,28 @@ class _ExpensesState extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void _openAddExpenseOerlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter ExpenseTracker'),
+        actions: [
+          IconButton(
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
+        ],
+      ),
       body: Column(
-        children: [Text('The Chart...'), Text('Expenses list...')],
+        children: [
+          const Text('The Chart...'),
+          Expanded(child: ExpensesList(expenses: _reqisteredExpenses))
+        ],
       ),
     );
   }
